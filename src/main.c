ad8ddcc9b4692cce9fb70f02c1d3f07ed819e2f3
Tingyu Ye <65630771+Tingyu-YE96@users.noreply.github.com>
2020-06-19 02:23:30 -0400

    Update main.c
    Making it as documented

--- src/main.c
+++ src/main.c
@@ -1,31 +1,33 @@
-#include <stdio.h>
-#include "../include/game/game.h"
-
-
-int main(void) {
+int main (void){
 	int board_size;
 	
-	generatePatterns();	
+	generate_patterns();	
 	
-	board_size = selectBoardSize();
+	board_size = select_board_size();
 	
-	/*Initailizing the Board*/
+	//Initailizing the Board
 	char board[board_size][board_size];
-	for (int i = 0; i < board_size; i++)
-		for (int j = 0; j < board_size; j++)
+	for (int i = 0; i < board_size; i++){
+		for (int j = 0; j < board_size; j++){
 			board[i][j] = 0;
-	displayBoard(board_size, board);
+		}
+	}
+	display_board(board_size, board);
 	
-	int winner = 0;
-	while(winner == 0)
-	{
-		playerMove(board_size, board, 1);
-		winner = checkWinner(board_size, board, 1);
-		isFullBoard(board_size, board);
-		playerMove(board_size, board, 2);
-		winner = checkWinner(board_size, board, 2);
-		isFullBoard(board_size, board);
+	int p = rand() % 2 + 1;
+	
+	int winner = 1;
+	while(winner){
+		player_move(board_size, board, p);
+		winner = check_winner(board_size, board, p);
+		is_full_board(board_size, board);
+		if (p == 1){
+			p = 2;
+		}
+		else{
+			p = 1;
+		}
 	}
 			
-    return 0;
+	return 0;
 }
