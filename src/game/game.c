49b17e050cf0416536a7c4129eb1d585eb53055a
Tingyu Ye <65630771+Tingyu-YE96@users.noreply.github.com>
2020-06-19 00:51:18 -0400

    Update game.c
    Adding the playerMove function

--- src/game/game.c
+++ src/game/game.c
@@ -1,14 +1,35 @@
 #include "../../include/game/game.h"
 
-/*
- * Todo: implement player_move and complete the description for this function, after completed, please delete todo.
- * @Author: Tingyu Ye,
- * @Parameter: int board_size, char board[][board_size], int player_name,
- * @Return: void,
- * @Description:
- */
-void player_move(int board_size, char board[][board_size], int player_name) {
-    ;
+void playerMove(int board_size, char board[][board_size], int player_name)
+{
+	int row, column, update;
+	int player_sign = -1;
+	if (player_name == 1)
+		player_sign = 1;	
+	PLAY_TURN: printf("\n!! Player %d turn!! \n!! Enter the Coordinates !!\n\nRow: ", player_name);
+	
+	scanf("%d", &row);
+	printf("Column: ");
+	scanf("%d", &column);
+	
+	printf("\n");
+	
+	if ( row >= board_size || row < 0 || column >= board_size || column < 0 )
+	{
+		printf("row = %d\ncolumn = %d",row,column);
+		printf("Enter the valid coordinates. \nThat is, Row and Column number should be between 0 and %d\n", board_size-1);
+		goto PLAY_TURN;
+	}
+	
+	update = updateBoard(row, column, board_size, board);
+	if(update)
+	{	goto PLAY_TURN;}
+	
+	board[row][column] = player_sign;
+	
+	displayBoard(board_size, board);
+	
+	return;
 }
 
 /*
