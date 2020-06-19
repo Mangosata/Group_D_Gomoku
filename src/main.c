008aaedc559941842f2f8aade44d3040d58a8261
Tingyu Ye <yty960414@gmail.com>
2020-06-19 00:35:25 -0400

    Adding the main function for intial release

--- src/main.c
+++ src/main.c
@@ -3,6 +3,29 @@
 
 
 int main(void) {
-
+	int board_size;
+	
+	generatePatterns();	
+	
+	board_size = selectBoardSize();
+	
+	/*Initailizing the Board*/
+	char board[board_size][board_size];
+	for (int i = 0; i < board_size; i++)
+		for (int j = 0; j < board_size; j++)
+			board[i][j] = 0;
+	displayBoard(board_size, board);
+	
+	int winner = 0;
+	while(winner == 0)
+	{
+		playerMove(board_size, board, 1);
+		winner = checkWinner(board_size, board, 1);
+		isFullBoard(board_size, board);
+		playerMove(board_size, board, 2);
+		winner = checkWinner(board_size, board, 2);
+		isFullBoard(board_size, board);
+	}
+			
     return 0;
 }
