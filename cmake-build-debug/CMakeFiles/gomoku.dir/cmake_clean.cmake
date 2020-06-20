aaa9aebbcf5848d8d3c0027c379d05cb1bdd219c
binjia <mangosata@gmail.com>
2020-06-20 15:08:15 -0400

    Complete the basic graphic.c including board and stone.

---
+++ cmake-build-debug/CMakeFiles/gomoku.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gomoku.dir/src/board/board.c.obj"
+  "CMakeFiles/gomoku.dir/src/game/game.c.obj"
+  "CMakeFiles/gomoku.dir/src/main.c.obj"
+  "gomoku.exe"
+  "gomoku.exe.manifest"
+  "gomoku.pdb"
+  "libgomoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gomoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
