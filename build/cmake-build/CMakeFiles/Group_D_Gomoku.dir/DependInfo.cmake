6968745acd2a1ca9231ab5517ff403ce33df9d3a
binjia <mangosata@gmail.com>
2020-06-22 21:05:43 -0400

    Updated .gitignore

---
+++ build/cmake-build/CMakeFiles/Group_D_Gomoku.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/Group_D_Gomoku/src/button_logic/button_logic.c" "F:/Group_D_Gomoku/cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/src/button_logic/button_logic.c.obj"
+  "F:/Group_D_Gomoku/src/game_logic/game_logic.c" "F:/Group_D_Gomoku/cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/src/game_logic/game_logic.c.obj"
+  "F:/Group_D_Gomoku/src/graphic/graphic.c" "F:/Group_D_Gomoku/cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/src/graphic/graphic.c.obj"
+  "F:/Group_D_Gomoku/src/main.c" "F:/Group_D_Gomoku/cmake-build-debug/CMakeFiles/Group_D_Gomoku.dir/src/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/msys64/mingw64/include/gtk-3.0"
+  "C:/msys64/mingw64/include/cairo"
+  "C:/msys64/mingw64/include/pango-1.0"
+  "C:/msys64/mingw64/include/fribidi"
+  "C:/msys64/mingw64/include/atk-1.0"
+  "C:/msys64/mingw64/include/pixman-1"
+  "C:/msys64/mingw64/include/freetype2"
+  "C:/msys64/mingw64/include/harfbuzz"
+  "C:/msys64/mingw64/include/libpng16"
+  "C:/msys64/mingw64/include/gdk-pixbuf-2.0"
+  "C:/msys64/mingw64/include/glib-2.0"
+  "C:/msys64/mingw64/lib/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
